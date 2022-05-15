34acaa801f16143f6c1cfd519423384ae98dd3fd
jabest <JessicaArwenBest@gmail.com>
2022-05-15 14:19:17 -0400

    Fixed variable setting ordering

--- index.php
+++ index.php
@@ -3,24 +3,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CiviCRM member query tool</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <style>
+        /* Rounded border */
+        hr.rounded {
+            border-top: 8px solid #bbb;
+            border-radius: 5px;
+        }
+    </style>
 </head>
-
-<body>
-<h1>CiviCRM member query tool</h1>
     
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <label>Last Name<br><label>
-      <input type="text" name="surname" value="<?php echo $surname;?>">
-  <br><br>
-  <label>Email<br><label>
-      <input type="text" name="email" value="<?php echo $email;?>">
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
- <?php 
-          
+ <?php   
      // define variables and set to empty values
     $surname = $email = "";
 
@@ -28,8 +20,31 @@
         $surname = $_POST["surname"];
         $email = $_POST["email"];
     }
+ ?> 
+    
+<body>
+    <h1>CiviCRM member query tool</h1>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <label>Last Name<br><label>
+        <input type="text" name="surname" value="<?php echo $surname;?>">
+            
+        <br><br>
+        
+        <label>Email<br><label>
+        <input type="text" name="email" value="<?php echo $email;?>">
+        
+        <input type="submit" name="submit" value="Submit">  
+    </form>
       
-    echo '<p>Hello ' . $surname . ' from ' . $email; 
+    <hr class="rounded">
+
+ <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo '<p>Hello ' . $surname . ' from ' . $email; 
+    }
 
  ?> 
 </body>
