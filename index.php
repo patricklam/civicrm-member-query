f204bf25a6ee34f00ecc7819744ba1c625460a5c
jabest <JessicaArwenBest@gmail.com>
2022-05-15 16:13:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -60,9 +60,11 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo '<p>Searching for ' . $surname . ' from ' . $email; 
         
-        /*  Wondering if this is reliant on the User Api key being logged in to a specific account...
+        
         $url = 'https://' . SITE . '/wp-json/civicrm/v3/rest?entity=contact&action=get&key=' . SERVER_API_KEY . '&api_key=' . USER_API_KEY . '&last_name=Lam';
         $contents = get_xml_from_url($url);
+        
+        /*  Wondering if this is reliant on the User Api key being logged in to a specific account...
 
         $contactxml = simplexml_load_string($contents);
 
